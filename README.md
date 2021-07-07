1c06006eb475093e69f12b2b75387f40389108be
Nick Maludy <nmaludy@gmail.com>
2021-07-07 11:06:15 -0400

    Added georender capability

--- README.md
+++ README.md
@@ -1,32 +1,145 @@
 # osmtiles
 
-Small python utility for converting lat/lon to and from OSM tile index used in a tile server URL.
+Small python utilities for working with OpenStreetMap tiles.
 
-## Understanding index format and zoom levels
+<!-- markdown-toc start - Don't edit this section. Run M-x markdown-toc-refresh-toc -->
+**Table of Contents**
+
+- [osmtiles](#osmtiles)
+    - [convert](#convert)
+        - [convert Usage](#convert-usage)
+        - [convert Examples](#convert-examples)
+    - [georender](#georender)
+        - [georender Usage](#georender-usage)
+        - [georender Examples](#georender-examples)
+    - [Reference and credit](#reference-and-credit)
+
+<!-- markdown-toc end -->
+
+```shell
+$ ./osmtiles.py --help
+usage: osmtiles.py [-h] {convert,georender} ...
+
+OpenStreetMap Tile Utilities
+
+positional arguments:
+  {convert,georender}
+    convert            Convert OpenStreetMap Tile ID to/from Lat/Lon
+    georender          Render all OpenStreetMap tiles in a lat/lon bounding
+                       box using the "render_list" utility from mod_tile.
+
+optional arguments:
+  -h, --help           show this help message and exit
+```
+
+## convert
+
+The `convert` command and can be used to convert lat/lon to and from
+OpenStreetMap tile index used by the HTTP `mod_tile` server.
+
+For an understanding of the index format and zoom levels, see:
 https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 
-## Usage
+### convert Usage
+
+```shell
+$ ./osmtiles.py convert --help
+usage: osmtiles.py convert [-h] [-t TILE] [-l LATLON] [-o OSM]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TILE, --tile TILE  Tile id in the format "zoom/x/y" (ex: "0/1/2")
+  -l LATLON, --latlon LATLON
+                        Lat/lon (decimal degrees) in the format "lat,lon,zoom"
+                        (ex: "1.23,4.56,3")
+  -o OSM, --osm OSM     zoom/lat/lon (decimal degrees) in the format
+                        "zoom/lat/lon" (ex: "3/1.23/4.56"). This format is
+                        what is present at the end of the URL from
+                        www.openstreemap.org
+```
+
+### convert Examples
 
 Convert from lat/lon zoom to tile index:
 ```shell
 # format: lat,lon,zoom
-$ python osmtiles.py -l 40.7707,-74.1508,10
+$ ./osmtiles.py convert -l 40.7707,-74.1508,10
 tile=10.0/301/384
 ```
 
 Convert from tile URL format to lat/lon zoom:
 ```shell
 # format: zoom/x/y
-$ python osmtiles.py -t 10/301/384
+$ ./osmtiles.py convert -t 10/301/384
 lat=40.97989806962013 lon=-74.1796875 zoom=10
 ```
 
 Convert from the format in the `openstreemap.org` URL, example: https://www.openstreetmap.org/#map=10/40.7707/-74.1508
 ```shell
 # format: zoom/lat/lon
-$ python osmtiles.py -o 10/40.7707/-74.1508
+$ ./osmtiles.py convert -o 10/40.7707/-74.1508
 tile=10/301/384
 ```
 
+## georender
+
+The `georender` command can be used to render all tiles in a geographic (lat/lon) bounding
+box. Under the hood we utilize the `render_list` utility from `mod_tile`, however this script
+does the conversion of lat/lon to tile index and then invokes `render_tile` for those
+limits at each zoom level.
+
+### georender Usage
+
+```shell
+$ ./osmtiles.py georender --help
+usage: osmtiles.py georender [-h] -x MIN_LON -X MAX_LON -y MIN_LAT -Y MAX_LAT
+                             -z MIN_ZOOM -Z MAX_ZOOM [-f] [-m MAP]
+                             [-l MAX_LOAD] [-s SOCKET] [-n NUM_THREADS]
+                             [-t TILE_DIR]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -x MIN_LON, --min-lon MIN_LON
+                        start longitude in decimal degrees, WGS84
+  -X MAX_LON, --max-lon MAX_LON
+                        end longitude in decimal degrees, WGS84
+  -y MIN_LAT, --min-lat MIN_LAT
+                        start latitude in decimal degrees, WGS84
+  -Y MAX_LAT, --max-lat MAX_LAT
+                        end latitude in decimal degrees, WGS84
+  -z MIN_ZOOM, --min-zoom MIN_ZOOM
+                        only render tiles greater or equal to this zoom level
+  -Z MAX_ZOOM, --max-zoom MAX_ZOOM
+                        only render tiles less than or equal to this zoom
+                        level
+  -f, --force           render tiles even if they seem current
+  -m MAP, --map MAP     render tiles in this map
+  -l MAX_LOAD, --max-load MAX_LOAD
+                        sleep if load is this high (default: 16)
+  -s SOCKET, --socket SOCKET
+                        unix domain socket name for contacting renderd
+  -n NUM_THREADS, --num-threads NUM_THREADS
+                        the number of parallel request threads (default: 1)
+  -t TILE_DIR, --tile-dir TILE_DIR
+                        tile cache directory
+```
+
+### georender Examples
+
+Render all tiles from zoom level 6 to 15 (inclusive) in the bounding box.
+```shell
+./osmtiles.py georender -z 6 -Z 15 -x 21.8 -X 40.7 -y 44.03 -Y 52.6
+```
+
+It is also possible to pass other command line options to `render_list`,
+such as `--num-threads 8` in order to parallelize the rendering or `--tile-dir`
+if your `mod_tile` cache directory is in a different location:
+```shell
+./osmtiles.py georender -z 6 -Z 15 -x 21.8 -X 40.7 -y 44.03 -Y 52.6 --num-threads 8 --tile-dir /opt/tile_server/mod_tile
+```
+
+
+
 ## Reference and credit
 - https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+- https://github.com/alx77/render_list_geo.pl
--- osmtiles.py
+++ osmtiles.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import argparse
+import datetime
 import math
+import os
 
 # Lon./lat. to tile numbers
 def deg2num(lat_deg, lon_deg, zoom):
@@ -10,6 +12,7 @@ def deg2num(lat_deg, lon_deg, zoom):
     ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
     return (xtile, ytile)
 
+
 # Tile numbers to lon./lat.
 def num2deg(xtile, ytile, zoom):
     n = 2.0 ** zoom
@@ -18,13 +21,12 @@ def num2deg(xtile, ytile, zoom):
     lat_deg = math.degrees(lat_rad)
     return (lat_deg, lon_deg)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='OpenStreetMap Tile<->Lat/lon translator')
-    parser.add_argument('-t', '--tile', help='Tile id in the format "zoom/x/y" (ex: "0/1/2")')
-    parser.add_argument('-l', '--latlon', help='Lat/lon (decimal degrees) in the format "lat,lon,zoom" (ex: "1.23,4.56,3")')
-    parser.add_argument('-o', '--osm', help='zoom/lat/lon (decimal degrees) in the format "zoom/lat/lon" (ex: "3/1.23/4.56"). This format is what is present at the end of the URL from www.openstreemap.org')
 
-    args = parser.parse_args()
+def minmax(x, y):
+    return (min(x, y), max(x, y))
+
+
+def convert(args):
     if args.tile:
         tile_parts = args.tile.split('/') # zoom/x/y
         x = int(tile_parts[1])
@@ -33,7 +35,7 @@ if __name__ == '__main__':
         lat, lon = num2deg(x, y, zoom)
         print('lat={} lon={} zoom={}'.format(lat, lon, zoom))
     elif args.latlon:
-        latlon_parts = args.tile.split(',') # lat,lon,zoom
+        latlon_parts = args.latlon.split(',') # lat,lon,zoom
         lat = float(latlon_parts[0])
         lon = float(latlon_parts[1])
         zoom = int(latlon_parts[2])
@@ -48,4 +50,139 @@ if __name__ == '__main__':
         print('tile={}/{}/{}'.format(zoom, x, y))
     else:
         print("ERROR: Need to specify either -t/--tile or -l/--latlon")
+        exit(1)
+
+
+def georender(args):
+    # number of tiles per metatile
+    BULK_SIZE = 8
+
+    # x
+    min_lon = args.min_lon
+    max_lon = args.max_lon
+    # y
+    min_lat = args.min_lat
+    max_lat = args.max_lat
+    # zoom
+    min_zoom = int(args.min_zoom)
+    max_zoom = int(args.max_zoom)
+
+    extra_args = []
+    if args.force:
+        extra_args.append("-f")
+    if args.map:
+        extra_args.extend(["-m", args.map])
+    if args.max_load:
+        extra_args.extend(["-l", str(args.max_load)])
+    if args.socket:
+        extra_args.extend(["-s", args.socket])
+    if args.num_threads:
+        extra_args.extend(["-n", str(args.num_threads)])
+    if args.tile_dir:
+        extra_args.extend(["-t", args.tile_dir])
+    extra_args_str = " ".join(extra_args)
+
+    # easier to just loop over each zoom level rather than trying to figure out
+    # and feed in all of the tile numbers to stdin
+    for zoom in range(min_zoom, max_zoom+1):
+        # convert lat/lon -> tile number
+        min_x, min_y = deg2num(min_lat, min_lon, zoom)
+        max_x, max_y = deg2num(max_lat, max_lon, zoom)
+
+        # aligning max range values to the border of meta-bundles
+        # (caused by internal bug of render_list)
+        max_x = (int(max_x/BULK_SIZE)+1)*BULK_SIZE-1
+        min_y = (int(min_y/BULK_SIZE)+1)*BULK_SIZE-1
+
+        # make sure min/max are in the right order
+        min_x, max_x = minmax(min_x, max_x)
+        min_y, max_y = minmax(min_y, max_y)
+
+        cmd = ("render_list -a {extra_args}"
+               " -z {min_z} -Z {max_z} -x {min_x} -X {max_x}"
+               " -y {min_y} -Y {max_y}").format(extra_args=extra_args_str,
+                                                min_z=zoom, max_z=zoom,
+                                                min_x=min_x, max_x=max_x,
+                                                min_y=min_y, max_y=max_y)
+
+        now = datetime.datetime.now().isoformat()
+        print("Zoom level {} began at: {}".format(zoom, now))
+        print(cmd)
+        os.system(cmd)
+        now = datetime.datetime.now().isoformat()
+        print("Zoom level {} ended at: {}".format(zoom, now))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='OpenStreetMap Tile Utilities')
+    subparsers = parser.add_subparsers(dest='command')
+
+    # convert
+    parser_convert = subparsers.add_parser('convert',
+                                           help='Convert OpenStreetMap Tile ID to/from Lat/Lon')
+    parser_convert.add_argument('-t', '--tile',
+                                help='Tile id in the format "zoom/x/y" (ex: "0/1/2")')
+    parser_convert.add_argument('-l', '--latlon',
+                                help=('Lat/lon (decimal degrees) in the format'
+                                      ' "lat,lon,zoom" (ex: "1.23,4.56,3")'))
+    parser_convert.add_argument('-o', '--osm',
+                                help=('zoom/lat/lon (decimal degrees) in the format'
+                                      ' "zoom/lat/lon" (ex: "3/1.23/4.56"). This format'
+                                      ' is what is present at the end of the URL from'
+                                      ' www.openstreemap.org'))
+
+    # georender
+    parser_georender = subparsers.add_parser('georender',
+                                             help=('Render all OpenStreetMap tiles in a'
+                                                   ' lat/lon bounding box using the "render_list"'
+                                                   ' utility from mod_tile.'))
+    parser_georender.add_argument('-x', '--min-lon',
+                                  help='start longitude in decimal degrees, WGS84',
+                                  required=True,
+                                  type=float)
+    parser_georender.add_argument('-X', '--max-lon',
+                                  help='end longitude in decimal degrees, WGS84',
+                                  required=True,
+                                  type=float)
+    parser_georender.add_argument('-y', '--min-lat',
+                                  help='start latitude in decimal degrees, WGS84',
+                                  required=True,
+                                  type=float)
+    parser_georender.add_argument('-Y', '--max-lat',
+                                  help='end latitude in decimal degrees, WGS84',
+                                  required=True,
+                                  type=float)
+    parser_georender.add_argument('-z', '--min-zoom',
+                                  help='only render tiles greater or equal to this zoom level',
+                                  required=True,
+                                  type=int)
+    parser_georender.add_argument('-Z', '--max-zoom',
+                                  help='only render tiles less than or equal to this zoom level',
+                                  required=True,
+                                  type=int)
+    parser_georender.add_argument('-f', '--force',
+                                  help='render tiles even if they seem current',
+                                  action='store_true')
+    parser_georender.add_argument('-m', '--map',
+                                  help='render tiles in this map')
+    parser_georender.add_argument('-l', '--max-load',
+                                  help='sleep if load is this high (default: 16)',
+                                  type=int)
+    parser_georender.add_argument('-s', '--socket',
+                                  help='unix domain socket name for contacting renderd')
+    parser_georender.add_argument('-n', '--num-threads',
+                                  help='the number of parallel request threads (default: 1)',
+                                  type=int)
+    parser_georender.add_argument('-t', '--tile-dir',
+                                  help='tile cache directory')
+
+    args = parser.parse_args()
+    if args.command == 'convert':
+        convert(args)
+    elif args.command == 'georender':
+        georender(args)
+    elif args.command == None:
+        parser.print_help()
+    else:
+        print("ERROR: Unknown command '{}'".format(args.command))
         exit(1)
